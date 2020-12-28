3cf1c1b8569ec1812e4c00f7c83485cb7b821e2a
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-28 15:14:41 +0900

    set function of add button

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,39 @@ import React , {useState} from 'react'
 import style from "./style.css";
 
 export const App = () => {
+  const [ todoText , setTodoText ] = useState("");
+  console.log(todoText);
+  const [ incompleteTodos, setIncompleteTodos ] = useState(["todo1","todo2"]);
+  const [ completeTodos,setCompleteTodos ] = useState(["完了したTODO"]);
 
-  const [incompleteTodos, setIncompleteTodos] = useState(["todo1","todo2"]);
-  const [completeTodos,setCompleteTodos] = useState(["完了したTODO"]);
+  const onClickAdd = (event)=> {
+    event.preventDefault();
+    if(todoText === "") return;
+    const newTodos = [...incompleteTodos,todoText];
+    //incompleteTodosに入力されている内容を分割代入で展開し、最後尾に新たに入力したtodoTextを追加し、新しい配列を作成する
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  }
+
+  const onChangeTodoText = (event)=> {
+    setTodoText(event.target.value)
+  }
+  
 
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力"/>
-        <button>追加</button>
+        <input 
+          type="text" 
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+          />
+        <button 
+          onClick={onClickAdd}
+        >
+          追加
+        </button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
